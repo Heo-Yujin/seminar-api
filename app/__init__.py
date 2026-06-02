99f45b1e2ca5b345a5c0e62c3cde901ffab7d4b5
Heo-Yujin <yujin0590@gmail.com>
2026-06-02 15:45:20 +0900

    fix: CORS 설정 추가 및 Docker 네트워크 문제 해결

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 from app.config import Config
 from app.db import close_db
 from app.routes import api_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
+    CORS(app)
     # 블루프린트 등록
     app.register_blueprint(api_bp)
     
